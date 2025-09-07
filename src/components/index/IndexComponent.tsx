05f84c3b81767d5fa896fe9fb66367bb32067f64
rizkypratamatan <rizkypratama@preloode.com>
2025-09-07 15:50:38 +0700

    Add team section

--- src/components/index/IndexComponent.tsx
+++ src/components/index/IndexComponent.tsx
@@ -1,6 +1,7 @@
 import Lottie from "lottie-react";
 import {Link} from "react-router";
 import Astronaut from "../../assets/json/astronaut.json";
+import CpuProcessor from "../../assets/json/cpu-processor.json";
 import SolarSystem from '../../assets/json/solar-system.json';
 
 
@@ -41,6 +42,73 @@ export default function IndexComponent() {
                     <svg className="w-full h-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
                         <path fill="white" d="M1000,4.3V0H0v4.3C0.9,23.1,126.7,99.2,500,100S1000,22.7,1000,4.3z"></path>
                     </svg>
+                </div>
+            </section>
+            <section className="container">
+                <div className="flex justify-center items-center">
+                    <Lottie className="basis-[3.25rem] h-[3.25rem]" animationData={CpuProcessor}/>
+                    <h2 className="font-bold">Our Hero Team</h2>
+                </div>
+                <h3 className="mb-13 font-brand text-[3.125rem] font-extrabold text-center">Expert with Tons of Experience</h3>
+                <div className="flex gap-8 items-center">
+                    <div className="basis-1/2 flex">
+                        <div className="team-profile">
+                            <img src="/resources/images/team-profile-1.jpg" alt="Rizky Pratama Picture"/>
+                            <div className="bg-dark-transparent">
+                                <div>
+                                    <h3>Rizky Pratama</h3>
+                                    <p>Fullstack Developer</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="team-profile">
+                            <img src="/resources/images/team-profile-2.jpg" alt="Cindy Nur Fajrin Picture"/>
+                            <div className="bg-dark-transparent">
+                                <div>
+                                    <h3>Cindy Nur Fajrin</h3>
+                                    <p>Frontend Developer</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="basis-1/2 flex flex-col gap-9">
+                        <p className="pl-6 border-l-2 border-white-dark">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                        <div className="team-skill">
+                            <h3>Web Development</h3>
+                            <div className="percentage">
+                                <div className="w-1/2"></div>
+                                <p className="left-[50%]">50%</p>
+                            </div>
+                        </div>
+                        <div className="team-skill">
+                            <h3>Desktop App Development</h3>
+                            <div className="percentage">
+                                <div className="w-1/2"></div>
+                                <p className="left-[50%]">50%</p>
+                            </div>
+                        </div>
+                        <div className="team-skill">
+                            <h3>Mobile App Development</h3>
+                            <div className="percentage">
+                                <div className="w-1/2"></div>
+                                <p className="left-[50%]">50%</p>
+                            </div>
+                        </div>
+                        <div className="team-skill">
+                            <h3>IOT Development</h3>
+                            <div className="percentage">
+                                <div className="w-1/2"></div>
+                                <p className="left-[50%]">50%</p>
+                            </div>
+                        </div>
+                        <div className="team-skill">
+                            <h3>Network Development</h3>
+                            <div className="percentage">
+                                <div className="w-1/2"></div>
+                                <p className="left-[50%]">50%</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
